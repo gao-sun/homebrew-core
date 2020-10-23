91366432558e2ea90a6bf00da34cbe0f2182a42d
Gao Sun <gaosun.dev@gmail.com>
2020-10-23 23:20:23 +0800

    Add swiftformat@0.46.2

---
+++ Formula/swiftformat@0.46.2.rb
@@ -0,0 +1,28 @@
+class SwiftformatAT0462 < Formula
+  desc "Formatting tool for reformatting Swift code"
+  homepage "https://github.com/nicklockwood/SwiftFormat"
+  url "https://github.com/nicklockwood/SwiftFormat/archive/0.46.2.tar.gz"
+  sha256 "cde87c93daba36e7c660412440f5775cf04d2c306bd716cafa0e52f085be4686"
+  license "MIT"
+  head "https://github.com/nicklockwood/SwiftFormat.git", shallow: false
+
+  depends_on xcode: ["10.1", :build]
+
+  def install
+    xcodebuild "-project",
+        "SwiftFormat.xcodeproj",
+        "-scheme", "SwiftFormat (Command Line Tool)",
+        "CODE_SIGN_IDENTITY=",
+        "SYMROOT=build", "OBJROOT=build"
+    bin.install "build/Release/swiftformat"
+  end
+
+  test do
+    (testpath/"potato.swift").write <<~EOS
+      struct Potato {
+        let baked: Bool
+      }
+    EOS
+    system "#{bin}/swiftformat", "#{testpath}/potato.swift"
+  end
+end
